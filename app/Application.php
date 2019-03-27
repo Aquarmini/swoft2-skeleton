e0e62e4426878a02bc08acc54e2e833ff6cb28e2
李铭昕 <715557344@qq.com>
2019-03-27 14:37:54 +0800

    Init

---
+++ app/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App;
+
+use Swoft\SwoftApplication;
+
+/**
+ * Application.
+ *
+ * It extends from [\Swoft\Core\SwoftApplication] which provides funtions.
+ *
+ * @since 2.0
+ */
+class Application extends SwoftApplication
+{
+}
---
+++ app/bean.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+return [];
