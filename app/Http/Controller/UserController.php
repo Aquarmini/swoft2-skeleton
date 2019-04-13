c252c62a9f99efc8fc6ba28c9e835d2706af1a06
李铭昕 <715557344@qq.com>
2019-04-13 17:32:09 +0800

    Update

---
+++ app/Http/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App\Http\Controller;
+
+use App\Models\User;
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+
+/**
+ * @Controller(prefix="/user")
+ */
+class UserController extends BaseController
+{
+    /**
+     * @RequestMapping(route="info")
+     */
+    public function info()
+    {
+        $user = User::find(1);
+
+        return $this->response->success($user->toArray());
+    }
+}
