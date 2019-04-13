a21e4e19ff9a5ebad8060ee12cda33ca13d91b6b
李铭昕 <715557344@qq.com>
2019-04-13 17:25:13 +0800

    Update

---
+++ app/Models/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App\Models;
+
+use Swoft\Db\Eloquent\Model as SwoftModel;
+
+class Model extends SwoftModel
+{
+}
