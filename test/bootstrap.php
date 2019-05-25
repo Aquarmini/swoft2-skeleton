4165bdd94d60443f5c44316a137136e1e4312e76
李铭昕 <715557344@qq.com>
2019-05-25 17:52:51 +0800

    Add phpunit.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,6 +10,10 @@ declare(strict_types=1);
  * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
  */
 
-return [
-    'name' => 'swoft framework 2.0',
-];
+error_reporting(E_ALL);
+
+! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
+
+\Swoole\Runtime::enableCoroutine(true);
+
+require BASE_PATH . '/vendor/autoload.php';
